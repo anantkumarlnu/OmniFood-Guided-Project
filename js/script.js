4810891c8e4d8b9e1e5f82b2385eed46194358b1
Anant Kumar <kumar.anant@northeastern.edu>
2025-07-15 12:44:54 -0400

    smoothscroll impl

--- js/script.js
+++ js/script.js
@@ -14,6 +14,51 @@ btnMobNavEl.addEventListener('click', function() {
   headerEl.classList.toggle("nav-open");
 })
 ///////////////////////////////////////////////////////////
+// smooth scrolling animation 
+const allLinks = document.querySelectorAll('a:link');
+//will only select links that have the href property 
+
+// adding an eventlistener to all the links 
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    //preventing the default behaviour so that we can then implement the smoothscrolling
+    e.preventDefault();
+
+    // getting the link out from the attribute of the anchor element that was clicked
+    const href = link.getAttribute("href");
+    // console.log(href);
+
+    // conditional statements to scroll to the top if any link that has # as href
+    if(href === '#')scrollTo({
+      top:0,
+      behavior:'smooth',
+    })
+
+    // for anchors that have links to some section now 
+    if(href!=="#" && href.startsWith('#')){
+      console.log(href);
+      // element that we want to scroll to 
+      const sectionEl = document.querySelector(href);
+      // now scrolling to that section 
+      sectionEl.scrollIntoView({
+        behavior:"smooth"
+      })
+    }
+
+    // closing mobile navigation when one of the navlinks are clicked 
+    // can do classlist.contains as well for this 
+    if(link.getAttribute("class")==="main-nav-link")
+      // removing navopen to close the mobile nav layover 
+    // cqn do remove from the class list of can do toggle 
+        headerEl.classList.toggle("nav-open");
+  })
+})
+
+
+
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
